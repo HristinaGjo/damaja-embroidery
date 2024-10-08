6ccf32f39eff2ecd930a034f6a92762b97da9b24
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-10-08 12:15:27 +0200

    hamburger menu instead of menu text for mobile

--- menu.js
+++ menu.js
@@ -1,31 +1,21 @@
 const menuToggle = document.getElementById('menuToggle');
 const overlay = document.getElementById('overlay');
-const logo = document.getElementById('logo').querySelector('a');
+const menuIcon = document.getElementById('menuIcon');
+const closeIcon = document.getElementById('closeIcon');
 
 // Toggle overlay menu on click
 menuToggle.addEventListener('click', () => {
     // Check if the overlay is active (open)
     if (overlay.classList.contains('active')) {
-        // Close the menu and go to homepage
+        // Close the menu
         overlay.classList.remove('active');
-        menuToggle.textContent = 'menu';  // Change the button back to 'Menu'
-        menuToggle.style.color = '#333';  // Set text color to black
-
-        /*logo.style.visibility = 'visible';*/
-        logo.style.color = '#333'; 
-        
-        // Redirect to homepage (change 'index.html' or '/' to your home URL if needed)
-        /*window.location.href = "https://hristinagjo.github.io/damaja-studio/";  // You can adjust the URL based on your homepage route*/
-
+        menuIcon.style.display = 'block';  // Show hamburger icon
+        closeIcon.style.display = 'none';   // Hide close icon
     } else {
         // Open the menu
         overlay.classList.add('active');
-        menuToggle.textContent = 'CLOSE';  // Change the button text to 'Close'
-        menuToggle.style.color = '#f2f2f2';  // Set text color to white
-        /*menuToggle.style.fontWeight = '200'; */
-
-        /*logo.style.visibility = 'hidden';*/
-        logo.style.color = '#000'; 
+        menuIcon.style.display = 'none';   // Hide hamburger icon
+        closeIcon.style.display = 'block';  // Show close icon
     }
 });
 
