6d435705129e415d8d3a87ef5f894bfb78964b53
Hristina Gjorgieva <hgjorgieva@gmail.com>
2024-10-09 17:23:50 +0200

    change logo color to white when the menu is open on mobile

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ const menuToggle = document.getElementById('menuToggle');
 const overlay = document.getElementById('overlay');
 const menuIcon = document.getElementById('menuIcon');
 const closeIcon = document.getElementById('closeIcon');
+const logo = document.getElementById('logo')
 
 // Toggle overlay menu on click
 menuToggle.addEventListener('click', () => {
@@ -10,12 +11,15 @@ menuToggle.addEventListener('click', () => {
         // Close the menu
         overlay.classList.remove('active');
         menuIcon.style.display = 'block';  // Show hamburger icon
-        closeIcon.style.display = 'none';   // Hide close icon
+        closeIcon.style.display = 'none'; 
+        logo.querySelector('a').style.color='#000'
+          // Hide close icon
     } else {
         // Open the menu
         overlay.classList.add('active');
         menuIcon.style.display = 'none';   // Hide hamburger icon
         closeIcon.style.display = 'block';  // Show close icon
+        logo.querySelector('a').style.color='#f2f2f2'
     }
 });
 
